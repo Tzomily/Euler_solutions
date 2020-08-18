a09852b592c0cdda099ecfbdcc4f43f9b83624e5
Tzomily <jomilyanv@gmail.com>
2020-08-18 18:55:58 +0300

    the solution of the problem2

---
+++ problem2/EvenSumFib.java
@@ -0,0 +1,25 @@
+package problem2;
+
+public class EvenSumFib {
+    public static int sumFib(){
+        int sum=0;
+        int [] fibNumbers= {1,2};
+        int n=2;
+        while(n< 4000000){
+            
+            if(n%2==0){
+                sum+=n;
+            }
+            n=fibNumbers[0]+fibNumbers[1];
+            fibNumbers[0]=fibNumbers[1];
+            fibNumbers[1]=n;
+        }
+
+        return sum;
+    }
+
+    public static void main(String[] args){
+        int sum = sumFib();
+        System.out.print(sum);
+    }
+}
