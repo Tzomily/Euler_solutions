f3f31a09b2aa8fd457d8528e2e3efc6a4fe4e656
Tzomily <jomilyanv@gmail.com>
2020-08-18 18:59:25 +0300

    fix linting issues

--- problem2/EvenSumFib.java
+++ problem2/EvenSumFib.java
@@ -1,24 +1,24 @@
 package problem2;
 
 public class EvenSumFib {
-    public static int sumFib(){
-        int sum=0;
-        int [] fibNumbers= {1,2};
-        int n=2;
-        while(n< 4000000){
-            
-            if(n%2==0){
-                sum+=n;
+    public static int sumFib() {
+        int sum = 0;
+        int[] fibNumbers = { 1, 2 };
+        int n = 2;
+        while (n < 4000000) {
+
+            if (n % 2 == 0) {
+                sum += n;
             }
-            n=fibNumbers[0]+fibNumbers[1];
-            fibNumbers[0]=fibNumbers[1];
-            fibNumbers[1]=n;
+            n = fibNumbers[0] + fibNumbers[1];
+            fibNumbers[0] = fibNumbers[1];
+            fibNumbers[1] = n;
         }
 
         return sum;
     }
 
-    public static void main(String[] args){
+    public static void main(String[] args) {
         int sum = sumFib();
         System.out.print(sum);
     }
