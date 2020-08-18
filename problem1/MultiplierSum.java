6e3abe967e701d90c1ba2eb2230dbcddc706a46d
Tzomily <jomilyanv@gmail.com>
2020-08-18 17:56:22 +0300

    Solution to problem 1

---
+++ problem1/MultiplierSum.java
@@ -0,0 +1,19 @@
+package problem1;
+
+public class MultiplierSum {
+    public static int SumofMultipliers() {
+        int sum = 0;
+        for (int i = 0; i < 1000; i++) {
+            if (i % 3 == 0 || i % 5 == 0) {
+                sum += i;
+            }
+        }
+        return sum;
+    }
+
+    public static void main(String[] args){
+        int sum= SumofMultipliers();
+        System.out.print(sum);
+    }
+    
+}
